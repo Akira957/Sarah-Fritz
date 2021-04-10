fdeec56aa607ba4061a39d2e2d0fc7d5f29b0a16
Robert Ramirez <metaknight757@gmail.com>
2021-04-10 17:38:26 -0500

    added front-end react and fixed typos in backend

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const app = express();
 
 app.use(cors());
 const PORT = process.env.PORT || 3001;
@@ -15,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static("Client/build"));
 }
 
 app.use(routes);
