167b138917f32aa953b351b9caeb634ab53fe7d5
Robert Ramirez <metaknight757@gmail.com>
2021-04-11 12:44:20 -0500

    added components and pages folder

---
+++ Client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const date = new Date();
+
+function Footer() {
+    <div>
+        <footer>
+            <p>Copyright@ {date.getFullYear()}</p>
+        </footer>
+    </div>
+}
+
+export default Footer;
---
+++ Client/src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from "../components/Footer";
+
+const date = new Date();
+
+function About() {
+    return (
+        <div>
+        <h1>About Sarah Fritz</h1>
+        <Footer />
+        </div>
+    )
+}
+
+export default About;
