64177cd4f3ce02a343f8a0c85ce9bdd88e2a968f
Robert Ramirez <metaknight757@gmail.com>
2021-04-19 22:29:33 -0500

    seeded database and updated the fields in the database

--- Models/art.js
+++ Models/art.js
@@ -3,10 +3,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ArtSchema = new Schema({
-    title: {type: String, required: true},
-    artist: {type: String, required: true},
-    category: {type: String, required: true},
-    image: {type: String, required: true}
+    title: { type: String, required: true },
+    category: { type: String, required: true },
+    image: { type: String, required: true }
 });
 
 const Art = mongoose.model("Art", ArtSchema);
--- Scripts/seedDB.js
+++ Scripts/seedDB.js
@@ -8,43 +8,36 @@ mongoose.connect(
 const artSeed = [
     {
         title: "Watercolor Print",
-        artist: "",
         category: "Print",
         image: "./images/Painting1"
     },
     {
         title: "Black and White Print",
-        artist: "",
         category: "Print",
         image: "./images/Painting2"
     },
     {
         title: "1800s Print",
-        artist: "",
         category: "Print",
         image: "./images/Painting3"
     },
     {
         title: "Glass Sculpture",
-        artist: "",
         category: "Glass Art",
         image: "./images/GlassArt"
     },
     {
         title: "Red White and Blue Print",
-        artist: "",
         category: "Print",
         image: "./images/Painting4"
     },
     {
         title: "Obsure Print",
-        artist: "",
         category: "Print",
         image: "./images/Painting5"
     },
     {
         title: "Town Painting",
-        artist: "",
         category: "Painting",
         image: "./images/Painting6"
     },
@@ -53,7 +46,7 @@ const artSeed = [
 db.Art
     .remove({}).then(() => db.Art.collection.insertMany(artSeed))
     .then(data => {
-        console.log(data.result.n + "records inserted");
+        console.log(data.result.n + " records inserted");
         process.exit(0);
     })
     .catch(err => {
