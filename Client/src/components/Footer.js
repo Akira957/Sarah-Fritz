e6ec00a0fba65ab351a65d1cc5ce74cc2b3d3d9d
Robert Ramirez <metaknight757@gmail.com>
2021-06-26 15:19:06 -0500

    added footer css file for the footer

--- Client/src/components/Footer.js
+++ Client/src/components/Footer.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import "./Footer.css";
 
 const date = new Date();
 
-const footerStyle = {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-}
+// const footerStyle = {
+//     display: "flex",
+//     alignItems: "center",
+//     justifyContent: "center"
+// }
 
-const pStyle = {
-    border: "3px solid blue",
-    textAlign: "center"
-}
+// const pStyle = {
+//     border: "3px solid blue",
+//     textAlign: "center"
+// }
 
 function Footer() {
     return (
         <div>
-            <footer style={footerStyle}>
-                <p style={pStyle}>Copyright@ {date.getFullYear()}</p>
+            <footer>
+                <p className="footer-title">Copyright@ {date.getFullYear()}</p>
             </footer>
         </div>
     )
